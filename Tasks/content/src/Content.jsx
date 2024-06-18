7de379f9c8231cc199fbf5ae741a405e371e7739
OliverWich <kontakt@oliver-wich.de>
2024-06-18 13:40:39 +0200

    Aufgabe 1

---
+++ Tasks/content/src/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+  return (
+    <main className="content">
+      <p>Content-Frontend</p>
+    </main>
+  );
+}
+
+export default Content;
---
+++ Tasks/header/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Header-Frontend</h1>
+      <button className="header-button">Klicken</button>
+    </header>
+  );
+}
+
+export default Header;
